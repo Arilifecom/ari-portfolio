d62dfcfd05a87ab877e4247273a0e7e7ee2aa5b1
Ari <arilife.com@gmail.com>
2025-03-21 17:39:26 +0900

    💄 Set Tailwind theme and CSS compornents

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
 import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-noto-sans-jp",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <main
+        className={`${montserrat.className} ${notoSansJP.className} bg-bg_primary w-full min-h-screen`}
+      >
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+    },
+    colors: {
+      bg_primary: "#E3F6F5",
+      bg_white: "#ffffff",
+      dark: "#272343",
+      light: "#ffffff",
+      blue: "#0AADBF",
+      pink: "#F107BA",
+      gray: "#C6C4C7",
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        mont: ["Montserrat", "sans-serif"],
+        noto: ["Noto Sans JP", "sans-serif"],
+      },
+      spacing: {
+        128: "8rem",
+        256: "16rem",
+      },
+      borderRadius: {
+        4: "4px",
+        8: "8px",
+        22: "22px",
       },
     },
   },
