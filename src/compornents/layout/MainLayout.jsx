5f22610979ec8cceaf347549138b670a2de66ae1
Ari <arilife.com@gmail.com>
2025-03-26 10:46:09 +0900

    🔥 Chamged main layout padding

--- src/compornents/layout/MainLayout.jsx
+++ src/compornents/layout/MainLayout.jsx
@@ -2,7 +2,7 @@
 const MainLayout = ({children, className=""}) => {
   return (
 
-    <div className={`w-full h-full min-h-[100vh] inline-block z-0 border-t-4 border-solid border-dark p-7 md:p-12 lg:p-32 bg-bg_white ${className}`}>
+    <div className={`w-full h-full inline-block z-0 border-t-4 border-solid border-dark p-7 pt-14 md:p-12 md:pt-20 lg:p-32 lg:pt-24 bg-bg_white ${className}`}>
       {children}
     </div>
   )
