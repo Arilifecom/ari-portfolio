a5deebac715cda37091729e34279ef6889e80f1b
Ari <arilife.com@gmail.com>
2025-03-27 16:07:30 +0900

    🩹 Add styles for screen xl size

--- src/compornents/AnimatedText.jsx
+++ src/compornents/AnimatedText.jsx
@@ -30,7 +30,7 @@ const sigleword = {
 const AnimatedText = ({text, className}) => {
   return (
     <div className='w-full mx-auto py-4 flex items-center justify-center text-center'>
-        <motion.h2 className={`inline-block w-full text-dark font-bold capitalize font-mont text-5xl md:text-6xl ${className}`}
+        <motion.h2 className={`inline-block w-full text-dark font-bold capitalize font-mont text-5xl md:text-6xl lg:text-8xl ${className}`}
         variants={quote}
         initial="initial"
         animate="animate"
