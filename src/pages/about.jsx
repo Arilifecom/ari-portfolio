b0a5972dbc0e9787b09d09a0531563dd92c274fc
Ari <arilife.com@gmail.com>
2025-03-27 16:14:47 +0900

    📝 Create About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,3 +1,4 @@
+import AnimatedNumber from "src/compornents/AnimatedNumuber";
 import CommonLayout from "src/compornents/layout/CommonLayout";
 import MainLayout from "src/compornents/layout/MainLayout";
 
@@ -6,8 +7,9 @@ export default function About() {
     <>
     <CommonLayout>
       <MainLayout>
-    <div>
+    <div className="flex flex-col justify-between xl:flex-row ">
         <h1 className="font-mont font-bold text-4xl sm:text-6xl md:text-7xl">About</h1>
+        <AnimatedNumber />
     </div>
       </MainLayout>
     </CommonLayout>
