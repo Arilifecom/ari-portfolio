d659fa3d965af9c8a60eac9aae0b767abd02af45
Ari <arilife.com@gmail.com>
2025-03-28 14:57:53 +0900

    💄 Add Icons

--- src/compornents/AnimatedCrackerPop.jsx
+++ src/compornents/AnimatedCrackerPop.jsx
@@ -1,6 +1,25 @@
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import { AriIcon, BlueStarIcon, ColorFigmaIcon, CrackerIcon, CssIcon, GreenCircleIcon, GreenHookIcon, GreenWaveIcon, HtmlIcon, JavaScriptIcon, NextJsIcon, PinkStarIcon, PurpleCircleIcon, PurpleLineIcon, PurpleWaveIcon, TailwindIcon, YellowCircleIcon, YellowStarIcon } from 'src/compornents/Icons';
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import {
+  AriIcon,
+  BlueStarIcon,
+  ColorFigmaIcon,
+  CrackerIcon,
+  CssIcon,
+  GreenCircleIcon,
+  GreenHookIcon,
+  GreenWaveIcon,
+  HtmlIcon,
+  JavaScriptIcon,
+  NextJsIcon,
+  PinkStarIcon,
+  PurpleCircleIcon,
+  PurpleLineIcon,
+  PurpleWaveIcon,
+  TailwindIcon,
+  YellowCircleIcon,
+  YellowStarIcon,
+} from "src/compornents/Icons";
 
 const AnimatIcons = [
   <PurpleLineIcon className="w-10 md:w-16 xl:w-20" />,
@@ -19,8 +38,8 @@ const AnimatIcons = [
   <HtmlIcon className="w-14 md:w-16 xl:w-18" />,
   <JavaScriptIcon className="w-16 md:w-20 xl:w-20" />,
   <TailwindIcon className="w-16 md:w-16 xl:w-20" />,
-  <NextJsIcon className="w-16 md:w-20 xl:w-20" />
-]
+  <NextJsIcon className="w-16 md:w-20 xl:w-20" />,
+];
 
 const AnimatedCrackerPop = () => {
   const [animationComplete, setAnimationComplete] = useState(false);
@@ -35,70 +54,70 @@ const AnimatedCrackerPop = () => {
     const { width, height } = container.getBoundingClientRect();
 
     // コンテナサイズ内でランダムな数値を取得
-    const randomX = randomNumberBetween(0, width - 64);  // 幅内でランダム
+    const randomX = randomNumberBetween(0, width - 64); // 幅内でランダム
     const randomY = randomNumberBetween(0, height - 64); // 高さ内でランダム
-    
+
     return { x: randomX, y: randomY };
   };
 
   useEffect(() => {
-    const timer = setTimeout(() => {      
-    }, 1000); 
-    return () => clearTimeout(timer)
-  },[animationComplete])
+    const timer = setTimeout(() => {}, 1000);
+    return () => clearTimeout(timer);
+  }, [animationComplete]);
 
-  return(
+  return (
     <>
-    <motion.div
-      initial={{ 
-        x: "-500px",
-        y: "500px",
-        opacity:0,
-      }}
-      animate={{ 
-        x: 10,
-        y: 10,
-        opacity:1,
-      }}
-      transition={{
-        type: "spring",
-        stiffness: 100,
-        damping: 10,
-        duration: 1,
-      }}
-       className="absolute bottom-0 -left-10 md:left-0 xl:-left-20"
-       onAnimationComplete={() => setAnimationComplete(true)}
-    >
-      <CrackerIcon  className="w-40 md:w-52 xl:w-60"/>
-    </motion.div>
+      <motion.div
+        initial={{
+          x: "-500px",
+          y: "500px",
+          opacity: 0,
+        }}
+        animate={{
+          x: 10,
+          y: 10,
+          opacity: 1,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+          duration: 1,
+        }}
+        className="absolute bottom-0 -left-10 md:left-0 xl:-left-20"
+        onAnimationComplete={() => setAnimationComplete(true)}
+      >
+        <CrackerIcon className="w-40 md:w-52 xl:w-60" />
+      </motion.div>
 
-    { animationComplete && (
-      <>
-      { AnimatIcons.map(( Icon, index) =>(
-        <motion.div
-          key={index}
-          initial={{
-            opacity:0 }}
-          animate={{
-            opacity: 1,
-            x: getRandomPosition().x,
-            y: -getRandomPosition().y,
-          }}
-          transition={{
-            type: "spring",
+      {animationComplete && (
+        <>
+          {AnimatIcons.map((Icon, index) => (
+            <motion.div
+              key={index}
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+                x: getRandomPosition().x,
+                y: -getRandomPosition().y,
+              }}
+              transition={{
+                type: "spring",
                 stiffness: 100,
                 damping: 10,
                 duration: 1,
-          }}
-          className="absolute bottom-0 left-0"
-        >
-          {Icon}
-        </motion.div>
-      ))}
-     </>
-    )}
+              }}
+              className="absolute bottom-0 left-0"
+            >
+              {Icon}
+            </motion.div>
+          ))}
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default AnimatedCrackerPop
+export default AnimatedCrackerPop;
