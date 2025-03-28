deb0a04cbb9e01b0ca44714bd89f2ff573e8f279
Ari <arilife.com@gmail.com>
2025-03-28 16:37:42 +0900

    💄 Add float keyframe animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 
@@ -38,6 +39,21 @@ module.exports = {
         4: "4px",
         8: "8px",
         22: "22px",
+      },
+      keyframes: {
+        sway: {
+          "0%, 100%": { transform: "rotate(30deg)" },
+          "50%": { transform: "rotate(-15deg)" },
+        },
+        float: {
+          "0%": { transform: "translateY(0%)" },
+          "50%": { transform: "translateY(-10%)" },
+          "100%": { transform: "translateY(10%)" },
+        },
+      },
+      animation: {
+        sway: "sway 2s ease-in-out infinite",
+        float: "float 1.8s ease-in-out infinite alternate-reverse",
       },
     },
   },
