e67ea82c35966cef6a35856778dbe66cb010bc41
Ari <arilife.com@gmail.com>
2025-04-08 17:14:46 +0900

    🩹 Changed flex style for md:size

--- src/compornents/Footer.jsx
+++ src/compornents/Footer.jsx
@@ -1,21 +1,30 @@
-import Link from "next/link"
-import SocialLinks from "src/compornents/SocialLinks"
+import Link from "next/link";
+import SocialLinks from "src/compornents/SocialLinks";
 
 const Footer = () => {
-  return(
+  return (
     <footer className="flex flex-col justify-center w-full border-t-4 border-solid border-dark font-mont font-bold  px-8 md:px-12 lg:px-32">
-        <div className="pt-8 pb-20 md:py-8 flex flex-col lg:flex-row gap-6 items-center justify-between">
-          <span className="">{new Date().getFullYear()}&copy; All Rights reserved. </span>
-          <SocialLinks />
-          <div className='flex items-center lg:py-2'>
-            build by <span className='text-pink text-2xl px-1'>&#9825;</span>
-            by&nbsp;<Link href="/" className='underline
+      <div className="pt-8 pb-20 md:py-8 flex flex-col md:flex-row gap-6 items-center justify-between">
+        <span className="">
+          {new Date().getFullYear()}&copy; All Rights reserved.{" "}
+        </span>
+        <SocialLinks />
+        <div className="flex items-center lg:py-2">
+          build by <span className="text-pink text-2xl px-1">&#9825;</span>
+          by&nbsp;
+          <Link
+            href="/"
+            className="underline
             underline-offset-2
-            ' target={"_blank"}>Ari</Link>
-            </div>
+            "
+            target={"_blank"}
+          >
+            Ari
+          </Link>
         </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
