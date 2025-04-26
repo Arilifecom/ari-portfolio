11f95544effca74dd9a1264ba965e984860ab12a
Ari <arilife.com@gmail.com>
2025-04-26 18:02:24 +0900

    🩹

--- src/compornents/BackToTopButton.jsx
+++ src/compornents/BackToTopButton.jsx
@@ -20,7 +20,7 @@ const BackToTopButton = () => {
   return isVisible ? (
     <button
       onClick={scrollToTop}
-      className="fixed bottom-6 right-6 p-3 rounded-full bg-blue-600 text-white shadow-lg hover:bg-blue-700 transition"
+      className="fixed bottom-12 md:bottom-8 right-6 p-3 rounded-full bg-blue-600 text-white shadow-lg hover:bg-blue-700 transition"
       aria-label="ページトップへ戻る"
     >
       <TopIcon className="w-8" />
