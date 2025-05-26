97ed0b832e744c8aee218be0a1f1d4706f9111bc
Ari <arilife.com@gmail.com>
2025-05-26 15:21:16 +0900

    🩹

--- src/compornents/layout/CommonLayout.jsx
+++ src/compornents/layout/CommonLayout.jsx
@@ -7,9 +7,11 @@ const CommonLayout = ({ children }) => {
     <div className="flex flex-col items-center bg-bg_primary mx-auto z-0">
       <div className="flex items-center justify-end w-full py-2 pr-2 md:pt-2">
         <AriIcon className={"w-6"} />
-        <h1 className="font-mont font-bold text-lg sm:text-xl md:text-2xl">
-          Ari's Portfolio
-        </h1>
+        <a href="/">
+          <h1 className="font-mont font-bold text-lg sm:text-xl md:text-2xl">
+            Ari's Portfolio
+          </h1>
+        </a>
       </div>
       <Header />
       {children}
